cd52bb757d6fc828e39c07b6289e8de9febc79e8
CampoNover16 <s.mackiewicz00@gmail.com>
2023-05-16 01:01:39 +0200

    added join/create board functions for users

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 from flask_session import Session
 from flask_mysqldb import MySQL
 from datetime import datetime
+import string, random
 
 app = Flask(__name__)
 
@@ -22,12 +23,11 @@ def index():
         return redirect("/login")
     else:
         cursor = mysql.connection.cursor()
-        print(session.get("user_id"))
-        cursor.execute("SELECT * FROM boards WHERE user_id=%s",(session.get("user_id"),))
+        cursor.execute("SELECT * FROM boards JOIN board_users ON boards.id=board_users.board_id WHERE board_users.user_id=%s",(session.get("user_id"),))
         user_board = cursor.fetchone()
         mysql.connection.commit()
         cursor.close()
-    return render_template('index.html', user=session['login'], board=user_board)
+    return render_template('index.html', user=session, board=user_board)
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
@@ -64,6 +64,50 @@ def login():
         else:
             flash('Given data are incorrect. Try again!', 'danger')
     return render_template('login.html')
+
+def createCodeToBoard():
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+
+@app.route('/joinBoard', methods=['POST','GET'])
+def joinBoard():
+    user_id = session.get("user_id")
+    message = None
+    req = request.get_json()
+
+    if session.get("login"):
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT id, name, invite_code FROM boards WHERE invite_code=%s and can_join=%s",(req['boardCode'],1))
+        checkUserBoard = cursor.fetchone()
+        if checkUserBoard:
+            cursor.execute("INSERT INTO board_users (board_id, user_id) VALUES(%s,%s)",(checkUserBoard[0], user_id))
+            cursor.execute("UPDATE boards SET can_join=%s WHERE id=%s",(0,checkUserBoard[0]))
+            mysql.connection.commit()
+            message = 'Succesfully joined to your friend\' board!'
+        else:
+            message = 'Something went wrong!'
+        cursor.close()
+    return {"message": message}
+
+@app.route('/createBoard', methods=['POST','GET'])
+def createBoard():
+    user_id = session.get("user_id")
+    message = None
+    default_board_name = "Default board name 1"
+    if session.get("login"):
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT * FROM boards JOIN board_users ON boards.id=board_users.board_id WHERE board_users.user_id=%s",(session.get("user_id"),))
+        checkUserBoard = cursor.fetchone()
+        if not checkUserBoard:
+            cursor.execute("INSERT INTO boards (name, invite_code, can_join) VALUES(%s,%s,%s)",(default_board_name, createCodeToBoard(), 1))
+            cursor.execute("SELECT id FROM boards ORDER BY ID DESC LIMIT 1")
+            findBoard = cursor.fetchone()
+            cursor.execute("INSERT INTO board_users (board_id, user_id) VALUES(%s,%s)",(findBoard[0], user_id))
+            mysql.connection.commit()
+            message = 'Succesfully created your board!'
+        else:
+            message = 'Something went wrong!'
+        cursor.close()
+    return {"message": message}
 
 @app.route('/logout')
 def logout():    
